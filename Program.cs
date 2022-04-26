d0ed475edbe351bae3cf8b47b1243feb859b7bbc
aliceif <aliceif@outlook.com>
2022-04-26 07:28:25 +0200

    add systemd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 Host.CreateDefaultBuilder (args)
+    .UseSystemd()
+    .ConfigureLogging((hostContext, builder) => {
+        // if we're in fact using systemd, throw out the default console logger and only use the systemd journald
+        if (Microsoft.Extensions.Hosting.Systemd.SystemdHelpers.IsSystemdService()) {
+            builder.ClearProviders();
+            builder.AddJournal(options => options.SyslogIdentifier = hostContext.Configuration["SyslogIdentifier"]);
+        }
+    })
     .ConfigureWebHostDefaults (webBuilder => {
         webBuilder.UseStartup<FuGetGallery.Startup> ();
     })
